df3ecc8f0e41000d3dbac42f93832a54d561c552
cristiano campos <cristiano.souzac@hotmail.com.br>
2020-10-25 01:23:47 -0300

    Exercicio Lista de Funcionarios

---
+++ ListaDeFuncionarios/ListaDeFuncionarios/Funcionarios.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace ListaDeFuncionarios
+{
+    class Funcionarios
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; private set; }
+
+        public Funcionarios(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void IncrementaSalario(double porcentage)
+        {
+            Salario += Salario * porcentage / 100;
+        }
+
+        public override string ToString()
+        {
+            return
+                "Id: "
+                + Id
+                + " Nome: "
+                + Nome
+                + " Salario: "
+                + Salario.ToString("F2", CultureInfo.InvariantCulture);
+
+
+        }
+    }
+}
---
+++ ListaDeFuncionarios/ListaDeFuncionarios/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ListaDeFuncionarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Quantos funcionários serão registrados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Funcionarios> list = new List<Funcionarios>();
+
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Funcionario " + i + ":");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Salário: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                list.Add(new Funcionarios(id, nome, salario));
+
+                Console.WriteLine();
+            }
+
+            Console.Write("Digite a identificação do funcionário que terá aumento de salário: ");
+            int pesquisaid = int.Parse(Console.ReadLine());
+
+            Funcionarios empresa = list.Find(x => x.Id == pesquisaid);
+
+            if (empresa != null)
+            {
+                Console.Write("Insira a porcentagem: ");
+                double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                empresa.IncrementaSalario(porcentagem);
+            }
+            else
+            {
+                Console.WriteLine("Este id não existe!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Lista de funcionários atualizada: ");
+
+            foreach (Funcionarios obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+        }
+    }
+}
