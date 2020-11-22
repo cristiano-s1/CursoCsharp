84d132ae6bb1425679797be03e497b3e60d06050
cristiano campos <cristiano.souzac@hotmail.com.br>
2020-11-22 17:46:49 -0300

    Exercicio conjunto

---
+++ Conjunto/Conjunto/Entities/LogRecord.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conjunto.Entities
+{
+    class LogRecord
+    {
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+
+        //GetHashCode
+        public override int GetHashCode()
+        {
+            //Retornar a igualdade do username
+            return Username.GetHashCode();
+        }
+
+
+        //Equals
+        public override bool Equals(object obj)
+        {
+            //Testar se o obj é do tipo object
+            if (!(obj is LogRecord))
+            {
+                return false;
+            }
+
+            //Tipo object
+            LogRecord other = obj as LogRecord;
+
+            //LogRecord é igual a outro LogRecord quando o Username for igual
+            return Username.Equals(other.Username);
+        }
+    }
+}
---
+++ Conjunto/Conjunto/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Conjunto.Entities;
+
+namespace Conjunto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Instanciando o conjunto com HashSet
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                //Ler um arquivo
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        //Fazer a leitura, retortar com espaço em branco, guardar no ventor de string
+                        string[] line = sr.ReadLine().Split(' ');
+
+                        //Nome ventor 0
+                        string name = line[0];
+
+                        //Data ventor 1
+                        DateTime instant = DateTime.Parse(line[1]);
+
+                        //Instanciando LogRecord
+                        set.Add(new LogRecord { Username = name, Instant = instant });
+                        Console.WriteLine(line);
+                    }
+
+                    //Contar a quantidade de usuários
+                    Console.WriteLine("Total users: " + set.Count);
+                }
+            }
+
+            //Exceção erro
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
